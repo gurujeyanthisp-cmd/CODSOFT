e8c7ef9ccc647f8db8556af25361ba9fd689d82e
gurujeyanthisp-cmd <gurujeyanthi.s.p@gmail.com>
2026-06-11 12:43:07 +0530

    Create Task3_RecommendationSystem.py

---
+++ Task3_RecommendationSystem.py
@@ -0,0 +1,36 @@
+# Task 3 - Recommendation System
+# CodeSoft AI Internship
+
+print("Welcome to the Movie Recommendation System!")
+
+# Movie categories
+action_movies = ["Avengers", "Batman", "John Wick"]
+comedy_movies = ["Mr. Bean", "Home Alone", "The Mask"]
+romantic_movies = ["Titanic", "The Notebook", "La La Land"]
+
+# Ask user preference
+print("\nChoose your favorite movie type:")
+print("1. Action")
+print("2. Comedy")
+print("3. Romantic")
+
+choice = input("\nEnter your choice (1/2/3): ")
+
+# Recommendation logic
+if choice == "1":
+    print("\nRecommended Action Movies:")
+    for movie in action_movies:
+        print("-", movie)
+
+elif choice == "2":
+    print("\nRecommended Comedy Movies:")
+    for movie in comedy_movies:
+        print("-", movie)
+
+elif choice == "3":
+    print("\nRecommended Romantic Movies:")
+    for movie in romantic_movies:
+        print("-", movie)
+
+else:
+    print("\nInvalid choice! Please run the program again.")
